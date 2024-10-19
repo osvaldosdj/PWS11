e42983235ff0bd2d2f0e250a9cf7840f516d0e18
osvaldo <osvaldosdj@gmail.com>
2024-10-19 13:22:08 -0300

    Final segundo dia

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'empresarios',
+    'investidores',
 ]
 
 MIDDLEWARE = [
--- empresarios/urls.py
+++ empresarios/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('cadastrar_empresa/', views.cadastrar_empresa, name="cadastrar_empresa"),
     path('listar_empresas/', views.listar_empresas, name="listar_empresas"),
+    path('empresa/<int:id>', views.empresa, name="empresa"),
+    path('add_doc/<int:id>', views.add_doc, name="add_doc"),
+    path('excluir_doc/<int:id>', views.excluir_doc, name="excluir_doc"),
+    path('add_metrica/<int:id>', views.add_metrica, name="add_metrica"),
+
 ]
